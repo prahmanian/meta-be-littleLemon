a8b2be2371b2aab1f7b93c96bddb629a4b69720b
pmrahmanian <111038493+pmrahmanian@users.noreply.github.com>
2023-03-30 08:38:16 -0500

    set up initial template and static files dirs

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "littlelemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# STATICFILES_DIRS = []
+STATICFILES_DIRS = ["static",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
